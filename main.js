c88003dfed0ef13974d778ab38643c75ebce0b41
BryanMurillo <bryanmiessler@gmail.com>
2022-12-24 00:47:09 -0500

    Agregando menu mobil

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navBarMail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const imgMenu = document.querySelector(".menu");
 
 navBarMail.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+imgMenu.addEventListener("click", toggleBurgerMenu);
+
+function toggleBurgerMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
