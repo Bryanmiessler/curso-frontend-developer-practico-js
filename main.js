afe8e47cf50e07d149685c3ffbaab3c09bd9e4c6
BryanMurillo <bryanmiessler@gmail.com>
2022-12-26 17:43:57 -0500

    agregando carrito compras

--- main.js
+++ main.js
@@ -2,15 +2,27 @@ const navBarMail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const imgMenu = document.querySelector(".menu");
+const iconCart = document.querySelector(".navbar-shopping-cart");
+const asideCart = document.querySelector(".product-detail");
 
 navBarMail.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
+  asideCart.classList.add("inactive");
   desktopMenu.classList.toggle("inactive");
 }
 
 imgMenu.addEventListener("click", toggleBurgerMenu);
 
 function toggleBurgerMenu() {
+  asideCart.classList.add("inactive");
   mobileMenu.classList.toggle("inactive");
+}
+
+iconCart.addEventListener("click", toggleAsideCart);
+
+function toggleAsideCart() {
+  desktopMenu.classList.add("inactive");
+  mobileMenu.classList.add("inactive");
+  asideCart.classList.toggle("inactive");
 }
